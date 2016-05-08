cbce9c7e0ab87cabf149f1b0a9cb7775d50706fa
JeanLLopes <jean.llopes@outlook.com>
2016-05-08 18:40:22 -0300

    Add Methods Access

--- SimpleExample-WebAPI/Controllers/ServicoController.cs
+++ SimpleExample-WebAPI/Controllers/ServicoController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using SimpleExample_WebAPI.Business;
+using SimpleExample_WebAPI.Models;
 
 namespace SimpleExample_WebAPI.Controllers
 {
@@ -14,7 +15,7 @@ namespace SimpleExample_WebAPI.Controllers
         private readonly ClientesBUS _clientesBus = new ClientesBUS();
 
         [HttpGet]
-        public HttpResponseMessage ConsultaCnpj()
+        public HttpResponseMessage ListaClientes()
         {
             try
             {
@@ -35,7 +36,37 @@ namespace SimpleExample_WebAPI.Controllers
             {
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, exception.Message);
             }
-            
+        }
+
+
+        public HttpResponseMessage ConsultaCnpj(String cnpj)
+        {
+            try
+            {
+                List<PedidoMOD> pedidosClientes = _clientesBus.GeraListaPedidos().FindAll(x => x.Cliente.Cnpj.Equals(cnpj));
+
+                if (pedidosClientes.Count > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, pedidosClientes);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.NotFound, pedidosClientes);
+
+            }
+            catch (SqlException exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, exception.Message);
+            }
+
+            catch (ApplicationException exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, exception.Message);
+            }
+
+            catch (Exception exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, exception.Message);
+            }
         }
 
     }
